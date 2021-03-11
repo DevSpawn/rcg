62ca08bdf2d8fa34663389ea8fc22aea8b120937
DevSpawn <timd3v@gmail.com>
2021-03-11 09:01:06 +0000

    upload project

---
+++ main.js
@@ -0,0 +1,123 @@
+const categories = [
+  "Sign Up",
+  "Credit Card Checkout",
+  "Landing Page (above the fold)",
+  "Calculator",
+  "App Icon",
+  "User Profile",
+  "Settings",
+  "404 page",
+  "Music Player",
+  "Social Share",
+  "Flash Message (Error/Success)",
+  "E-Commerce Shop (Single Item)",
+  "Direct Messaging",
+  "Countdown Timer",
+  "On/Off Switch",
+  "Pop-Up / Overlay",
+  "Email Receipt",
+  "Analytics Chart",
+  "Leaderboard",
+  "Location Tracker",
+  "Home Monitoring Dashboard",
+  "Search",
+  "Onboarding",
+  "Boarding Pass",
+  "TV App",
+  "Subscribe",
+  "Dropdown",
+  "Contact Us",
+  "Map",
+  "Pricing",
+  "File Upload",
+  "Crowdfunding Campaign",
+  "Customize Product",
+  "Car Interface",
+  "Blog Post",
+  "EMI Calculator",
+  "Weather",
+  "Calendar",
+  "Testimonials",
+  "Dribbble Invite",
+  "Workout Tracker",
+  "ToDo List",
+  "Food/Drink Menu",
+  "Favorites",
+  "Info Card",
+  "Invoice",
+  "Activity Feed",
+  "Coming Soon",
+  "Notifications",
+  "Job Listing",
+  "Press Page",
+  "Daily UI logo",
+  "Header Navigation",
+  "Confirm Reservation",
+  "Icon Set",
+  "Breadcrumbs",
+  "Video Player",
+  "Shopping Cart",
+  "Background Pattern",
+  "Color Picker",
+  "Redeem Coupon",
+  "Workout of the day",
+  "Best of 2015",
+  "Select User Type",
+  "Notes Widget",
+  "Statistics",
+  "Hotel Booking",
+  "Flight Search",
+  "Trending",
+  "Event Listing",
+  "Schedule",
+  "Image Slider",
+  "Virtual Reality",
+  "Download App",
+  "Pre-Order",
+  "Loading...",
+  "Thank You",
+  "Pending Invitation",
+  "Itinerary",
+  "Date Picker",
+  "Status Update",
+  "Form",
+  "Button",
+  "Badge",
+  "Pagination",
+  "Progress Bar",
+  "Tooltip",
+  "Avatar",
+  "Terms of Service",
+  "Create New",
+  "Curated For You",
+  "F.A.Q.",
+  "Splash Screen",
+  "News",
+  "Product Tour",
+  "Currently In-Stock",
+  "Giveaway",
+  "Advertisement",
+  "Categories",
+  "Redesign Daily UI Landing Page",
+];
+
+let i = 0;
+let speed = 100;
+
+const loopAndSelect = () => {
+  setTimeout(() => {
+    randomNumber = Math.floor(Math.random() * categories.length);
+    document.getElementById("category").innerHTML = categories[randomNumber];
+    i ++;
+    if (i < 30){
+      generate();
+      speed += 5;
+      console.log(speed);
+    }
+  }, speed)
+}
+
+const generate = () => {
+  document.getElementById("generate").disabled = true;
+  loopAndSelect();
+}
